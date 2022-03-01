0679ac13b8397a1755118c286a7b4149e55e1df8
MdNadiruzzamanNibras <nadiruzzamannibras@gmail.com>
2022-03-01 18:02:21 +0600

    set detail created

--- js/main.js
+++ js/main.js
@@ -11,21 +11,23 @@ const searchBtn = ()=>{
 const showResult = phones =>{
     
     const brandDetail =document.getElementById('brands');
+    brandDetail.textContent='';
     phones.slice(0, 20).forEach(phone=>{
-      
+    
         const div = document.createElement('div')
         div.classList.add('col-lg-4');
         div.innerHTML = `<div class="card border p-5">
         <div class="pro-pic ">
-            <img class="w-30" src="${phone.image}" alt="">
+            <img class="w-30 img-fluid" src="${phone.image}" alt="">
         </div>
         <h2>Name: ${phone.phone_name}</h2>
         <h5>Brand: ${phone.brand}</h5>
-        <p></p>
+       
         <div class="all-button">
-            
             <button onclick="details('${phone.slug}')" class="btn btn-success">Details</button>
-        </div>`
+            </div>
+            </div>
+        `
         brandDetail.appendChild(div)
     })
    
@@ -36,3 +38,28 @@ const details=(id)=>{
     .then(res=> res.json())
     .then(data=> setDetails(data.data))
  }
+ const setDetails=(info)=>{
+   
+  const details =document.getElementById("details")
+  details.textContent='';
+  details.innerHTML=`
+  <div class="px-auto center mx-auto">
+        <div class="pro-pic ">
+            <img class="w-30 img-fluid" src="${info.image}" alt="">
+        </div>
+        <h2>Name: ${info.name}</h2>
+        <p> Release Date:${info.releaseDate}</p>
+        <h4>Brand: ${info.brand}</h4>
+        <div class="d-fex justify-content-center"><div> <h6> Memory Storage <br> 
+        Capacity:</h6></div> 
+        <div><P>${info.mainFeatures.storage}  </p></div>
+        </div>
+        <h6>Sensor:</h6>
+        <P>${info.mainFeatures.sensors}</p>
+        <h6>Others information:</h6>
+        <p> ${info.others.WLAN}
+       </div>
+
+  `
+  console.log(info)
+}
