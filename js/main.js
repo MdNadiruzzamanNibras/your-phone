9dab20c7ab750675d3b379d1fce1c3e84e6b7e7f
MdNadiruzzamanNibras <nadiruzzamannibras@gmail.com>
2022-03-01 14:27:29 +0600

    cerated brand details

--- js/main.js
+++ js/main.js
@@ -5,9 +5,28 @@ const searchBtn = ()=>{
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
     fetch(url)
     .then(res=> res.json())
-    .then(data=> showResult(data))
+    .then(data=> showResult(data.data))
 }
 
-const showResult = brands =>{
-    console.log(brands)
+const showResult = phones =>{
+    
+    const brandDetail =document.getElementById('brands');
+    phones.slice(0, 20).forEach(phone=>{
+      
+        const div = document.createElement('div')
+        div.classList.add('col-lg-4');
+        div.innerHTML = `<div class="card border p-5">
+        <div class="pro-pic ">
+            <img class="w-30" src="${phone.image}" alt="">
+        </div>
+        <h2>Name: ${phone.phone_name}</h2>
+        <h5>Brand: ${phone.brand}</h5>
+        <p></p>
+        <div class="all-button">
+            
+            <button onclick="details('${phone.slug}')" class="btn btn-success">Details</button>
+        </div>`
+        brandDetail.appendChild(div)
+    })
+   
 }
